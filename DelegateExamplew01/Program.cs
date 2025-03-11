1b6c4c77b4ead803a548a4ca30ff9d7d15fe1992
lab_services_student <lab_services_student@lab7L95SR>
2025-03-11 16:17:08 +0200

    Add project files.

---
+++ DelegateExamplew01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExamplew01
+{
+   public class Program
+    {
+        // define a delegate
+        public delegate int MathOperations(int a, int b);
+
+        // Define methods that match delegate signature
+        public static int Add(int a, int b) => a + b;
+
+        public static int Subtract(int a, int b) => a - b;  // Push To GitHub
+        
+        public static void Main()
+        {
+            // Assign methods to delegate
+            MathOperations operation = Add;
+            Console.WriteLine("Addition: "+ operation(5,3));
+
+            operation = Subtract;
+            Console.WriteLine("Subtraction: "+ operation(5,3));
+        }
+    }
+}
